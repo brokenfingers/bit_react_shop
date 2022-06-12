814ea2143b61aa1d0a4087f229dd451cb3cc11e3
brokenfingers <buvotrys@gmail.com>
2022-06-12 18:27:55 +0300

    back createContext

--- my-app/src/Components/back/Back.jsx
+++ my-app/src/Components/back/Back.jsx
@@ -1,10 +1,15 @@
 import '../../bootstrap.css';
 import '../../back.scss';
+import BackContext from '../../Contexts/BackContext';
 function Back() {
   return (
+    <BackContext.Provider value={{}}>
+
     <h1>
         Back
     </h1>
+
+    </BackContext.Provider>
   );
 }
 
