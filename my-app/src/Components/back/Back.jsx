3b0d3304d1ff3fffe8f8ee758a6bf87fd4c77304
brokenfingers <buvotrys@gmail.com>
2022-06-12 20:27:40 +0300

    read

--- my-app/src/Components/back/Back.jsx
+++ my-app/src/Components/back/Back.jsx
@@ -4,8 +4,14 @@ import BackContext from '../../Contexts/BackContext';
 import Navbar from './Navbar';
 import { useState } from 'react';
 import ProductList from './ProductList'
+import { useEffect } from 'react';
+import axios from 'axios';
 function Back() {
   const [products, setProducts] = useState(null)
+  useEffect(() => {
+    axios.get('http://localhost:3003/admin/products')
+      .then(res => setProducts(res.data))
+  }, [])
   return (
     <BackContext.Provider value={{ products }}>
       <div className="container">
