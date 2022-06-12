d06d44138cca3e1da8da86fc01f76cffe1dc3f76
brokenfingers <buvotrys@gmail.com>
2022-06-12 21:49:51 +0300

    product list

--- my-app/src/Components/back/ProductLine.jsx
+++ my-app/src/Components/back/ProductLine.jsx
@@ -1,10 +1,31 @@
-function ProductLine({ product }) {
+function classNameName({ product }) {
 
     return (
         <li className='list-group-item'>
-            {product.title}
+            <div className="product-line_content">
+                <div className="product-line_content_top">
+                    <div className="product-line_image">
+                        <img src="https://picsum.photos/200/300" alt='random generated' />
+                    </div>
+                    <div className="product-line_title">
+                        {product.title}
+                    </div>
+                    <div className="product-line_code">
+                        {product.code}
+                    </div>
+                    <div className="product-line_price">
+                        {product.price}
+                    </div>
+                </div>
+                <div className="product-line_content_bottom">
+                    {product.description}
+                </div>
+            </div>
+            <div className="product-line_buttons">
+
+            </div>
         </li>
     )
 }
 
-export default ProductLine
+export default classNameName
